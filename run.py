ef8935d7c674414e0767e3189670e3828d66af72
rubengardner <rgardnercuesta@gmail.com>
2022-09-28 08:31:51 +0000

    Add function that inputs board size from user

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_function():
+    """
+    Welcome function which salutes the user
+    """
+    print("Welcome to this exciting match of the classic battleship game!")
+    print("In this version, you will play against the machine, you will have the chance to choose how big you want your board and how many ships you would like.")
+    
+
+def input_board_size():
+    """
+    Inputs the user to choose the board size
+    """
+    print("Now you will choose the size of the board, it doesn't have to be a square, but rows and columns can be a maximun of 8 each/n")
+    print("Please, choose number of rows (1 to 8):") 
+    rows = input()
+    print("Please, choose number of columns (1 to 8):") 
+    columns = input()

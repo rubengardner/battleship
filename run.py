5fd12e12cba83fb17c90127b426884a190619ba4
rubengardner <rgardnercuesta@gmail.com>
2022-09-28 11:35:29 +0000

    Connect input and validation of board data through main() function

--- run.py
+++ run.py
@@ -10,30 +10,48 @@ def input_board_size():
     """
     Inputs the user to choose the board size
     """
+    board_size = []
     print("Now you will choose the size of the board, it doesn't have to be a square, but rows and columns can be a maximun of 8 each.\n")
-    print("Please, choose number of rows (1 to 8):") 
-    rows = input()
-    print("Please, choose number of columns (1 to 8):") 
-    columns = input()
+    while True:
+        print("Please, choose number of rows (1 to 8):") 
+        row = input()
+        if validate_board_size(row):
+            break
 
-def validate_board_size(row, column):
+    while True:
+        print("Please, choose number of columns (1 to 8):") 
+        col = input()
+        if validate_board_size(col):
+            break
+
+
+
+def validate_board_size(data):
     """
     Validates board size data by converting data into integer. 
     Raises value error if value entered is not between 1 and 8.
     Also raises a value error if the value is not an integer.
     """
     try:
-        check_row = int(row)
-        check_col = int(column)
-        if check_row >= 9 and check_row <= 0:
+        check_data = int(data)
+        if check_data >= 9 or check_data <= 0:
             raise ValueError(
-                f"Row value must be between 1 and 8, you provided {check_row}"
+                f"Value must be between 1 and 8, you provided {check_data}"
             )
-        if check_col >= 9 and check_col <= 0:
-            raise ValueError(
-                f"Row value must be between 1 and 8, you provided {check_col}"
-            )
+        
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
+    return True
+
+
+
+
+
+
+def main():
+    welcome_function()
+    input_board_size()
+
+main()
 

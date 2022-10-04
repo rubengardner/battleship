d420ed36f5781ade60f7d1b26e04b9cde0d59f23
rubengardner <rgardnercuesta@gmail.com>
2022-10-04 07:15:44 +0000

    Add playing mechanics in main function

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ def welcome_function():
     Welcome function which salutes the user
     """
     print("Welcome to this exciting match of the classic battleship game!")
-    print("In this version, you will play against the machine, you will have the chance to choose how big you want your board and how many ships you would like.")
+    print("In this version, you will play against the machine,")
+    print("you will have the chance to choose the size of the board and the amount of ships.")
     # adding a path to instructions functions
 
 
@@ -70,11 +71,6 @@ def input_fleet_size():
     return int(boats)
 
 def validate_boat_fleet(data):
-    """
-    Validates boat fleet size by converting data into integer. 
-    Raises value error if value entered is not between 3 and 8.
-    Also raises a value error if the value is not an integer.
-    """
     try:
         check_data = int(data)
         if check_data >= 9 or check_data <= 2:
@@ -89,17 +85,15 @@ def validate_boat_fleet(data):
 class Board:
     """
     Class that creates the boards for the user and the enemy
-    Store all the information related to the game: number of boats, 
-    size of the board, guesses, misses...
     """
     def __init__(self, size, boats):
         self.size = size
         self.boats = boats
-        self.board = [['O' for x in range(self.size[1])] for y in range(self.size[0])]
+        self.board = [['*' for x in range(self.size[1])] for y in range(self.size[0])]
         self.guess = []
         self.miss = []
         self.boat_placement = []
-        self.last_shot = "No shot taken"
+        self.last_shot='No last shot'
     
     def user_print(self): 
         for boat in self.boat_placement:
@@ -110,7 +104,8 @@ class Board:
             self.board[miss[0]][miss[1]] = '0'
         for row in self.board:
             print(" ".join(row))
-    
+        
+
     def enemy_print(self):
         for guess in self.guess:
             self.board[guess[0]][guess[1]] = 'X'
@@ -118,74 +113,57 @@ class Board:
             self.board[miss[0] - 1][miss[1]] = '0'
         for row in self.board:
             print(" ".join(row))
+        
 
     def random_boat_selection(self):
-        """
-        Selects randomnly the placement of the boats on the board.
-        """
-        #Converting index user friendly to python friendly
-        index1 = self.size[0] - 1
+        index1 = self.size[0] - 1 #Converting index user friendly to python friendly
         index2 = self.size[1] - 1
-        #Loops through all the number of boats to place
+        
         for boat in range(self.boats):
-            #Checks if the coordinates are already taken
             while True:
                 rand1 = random.randint(0, index1)
                 rand2 = random.randint(0, index2)
                 if self.validate_boat_placement(rand1, rand2):
-                    print(f"Boat nº {boat + 1} placed!")
-                    print(f"row = {rand1}, col = {rand2}")
-                    self.board[rand1][rand2] = 'X'
                     break
 
         print(f"All {self.boats} boats placed!")
 
     def manual_boat_placement(self):
-        """
-        Permits user to select manually the location of the boats, 
-        checking the input is an integer, in the correct range and that
-        the location isn't already taken.
-        """
         print("Choose the location of the boats.")
         print("Remember, you're grid is composed of: ")
         print(f"{self.size[0]} rows.")
         print(f"{self.size[1]} columns.\n")
-        # Loops through all the number of boats to place
+        
         for boat in range(self.boats):
-            # Loops until user gives a correct location for 1 boat
             while True:
+                print(f"Please, input boat nº {boat + 1} coordinates:")
                 while True:
-                    print(f"Choose row to place boat (Must be between 1 and {self.size[0]}):")
+                    print(f"Row (Must be between 1 and {self.size[0]}):")
                     row_selection = input()
-                    # Validates if the input is an integer
                     if self.validate_integer_input(row_selection):
-                        # Validates if the input is inside the board
                         if self.validate_integer_range(int(row_selection), self.size[0]):
                             break
 
                 while True:
-                    print(f"Choose column to place boat (Must be between 1 and {self.size[1]}):")
+                    print(f"Column(Must be between 1 and {self.size[1]}):")
                     column_selection = input()
-                    # Validates if the input is an integer
                     if self.validate_integer_input(column_selection):
-                        # Validates if the input is inside the board
                         if self.validate_integer_range(int(column_selection), self.size[1]):
                             break
-                # Validates if the location is already taken          
-                if self.validate_boat_placement(int(row_selection), int(column_selection)):
-                    print(f"Boat nº {boat + 1} placed!")
-                    self.board[int(row_selection) - 1][int(column_selection) - 1] = 'X'
+
+                if self.validate_boat_placement(int(row_selection) - 1, int(column_selection) - 1):
+                    os.system("cls" if os.name == "nt" else "clear")
+                    print(f"Boat nº {boat + 1} placed!\n")
                     break
                 else: 
-                    print("There's already a boat in the location:")
-                    print(f"Row:{row_selection}. Column: {column_selection} ")
+                    os.system("cls" if os.name == "nt" else "clear")
+                    print("There's already a boat in that location:")
+                    print(f"Row:{row_selection}. Column: {column_selection}\n ")
 
+        os.system("cls" if os.name == "nt" else "clear")
         print("All boats placed correctly!")
 
     def validate_integer_input(self, data):
-        """
-        Validates if the input is an integer
-        """
         try:
             check_data = int(data)
         except ValueError as e:
@@ -194,13 +172,10 @@ class Board:
         return True
             
     def validate_integer_range(self, data, size):   
-        """
-        Validates if the input is inside the board
-        """
         try:
             if size < data or data <= 0:
                 raise ValueError(
-                f"Value must be between {size} and 0, you provided {data}"
+                f"Value must be between {size} and 1 (including both), you provided {data}"
                 )       
         except ValueError as e:
             print(f"Invalid data: {e}, please try again.\n")
@@ -208,53 +183,14 @@ class Board:
         return True    
 
     def validate_boat_placement(self, row, col):
-        """
-        Validates if there is already a boat in that location
-        """
+    
         coordinates = [row, col]
         for boat in self.boat_placement:
             if coordinates == boat:
                 return False  
         self.boat_placement.append(coordinates)            
         return True
-
-    def random_boat_selection(self):
-        index1 = self.size[0] - 1 #Converting index user friendly to python friendly
-        index2 = self.size[1] - 1
         
-        for boat in range(self.boats):
-            while True:
-                rand1 = random.randint(0, index1)
-                rand2 = random.randint(0, index2)
-                if self.validate_boat_placement(rand1, rand2):
-                    break
-
-        print(f"All {self.boats} boats placed!")
-        
-    def automatic_shot_location(self):
-        index1 = self.size[0] - 1 #Converting index user friendly to python friendly
-        index2 = self.size[1] - 1
-        while True:
-            rand1 = random.randint(0, index1)
-            rand2 = random.randint(0, index2)
-            if self.validate_shots_taken(rand1, rand2):
-                break
-
-    def validate_shots_taken(self, row, col):
-        if [row,col] in self.guess:
-            print('You already shot this location')
-            return False
-        elif [row, col] in self.miss:
-            print('You already shot this location')
-            return False
-        elif [row, col] in self.boat_placement:
-            self.last_shot = 'HIT!'
-            self.guess.append([row, col])
-            return True
-        else:
-            self.last_shot = "Miss"
-            self.miss.append([row, col])
-            return True
 
     def input_shot_location(self):
 
@@ -279,15 +215,68 @@ class Board:
             if self.validate_shots_taken(row_shooting - 1, col_shooting - 1):
                 break
                
+    def automatic_shot_location(self):
+        index1 = self.size[0] - 1 #Converting index user friendly to python friendly
+        index2 = self.size[1] - 1
+        while True:
+            rand1 = random.randint(0, index1)
+            rand2 = random.randint(0, index2)
+            if self.validate_shots_taken(rand1, rand2):
+                break
+            
+
+
+    def validate_shots_taken(self, row, col):
+        if [row, col] in self.guess:
+            print('You already shot this location')
+            return False
+        elif [row, col] in self.miss:
+            print('You already shot this location')
+            return False
+        elif [row, col] in self.boat_placement:
+            self.last_shot = 'HIT!'
+            self.guess.append([row ,col])
+            return True
+        else:
+            self.last_shot = "Miss"
+            self.miss.append([row, col])
+            return True
+  
 def main():
     welcome_function()
     board_size = input_board_size()
     fleet_size = input_fleet_size()
+    
     user_board= Board(board_size, fleet_size)
-    # computer_board = board(board_size)
-    user_board.print()
+    enemy_board = Board(board_size, fleet_size)
+
+
     user_board.manual_boat_placement()
-    user_board.print()
+
+    enemy_board.random_boat_selection()
+    
+    os.system("cls" if os.name == "nt" else "clear")
+    while True:
+        if len(enemy_board.guess) == fleet_size and len(user_board.guess) == fleet_size:
+            print("Draw")
+            return False
+        elif len(enemy_board.guess) == fleet_size:
+            print("Win")
+            return False
+        elif len(user_board.guess) == fleet_size:
+            print("Loose")
+            return False
+        else:
+            print("User board:")
+            user_board.user_print()
+            print(f"Last enemy shot was: {user_board.last_shot} ")
+            print("\n")
+            print("Enemy board:")
+            enemy_board.user_print()
+            print(f"Your last shot was: {enemy_board.last_shot} ")
+            enemy_board.input_shot_location()
+            user_board.automatic_shot_location()
+            os.system("cls" if os.name == "nt" else "clear")
 
 
 
